e500d9905028bd446c4e4a5f0285310a61ab0199
Alexander <alex.galev@yahoo.com>
2023-02-26 11:02:23 -0700

    practiced Context

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,33 @@
-import React from "react";
-import ThemedButton from "./ThemedButton";
-import DarkModeToggle from "./DarkModeToggle";
-import defaultUser from "../data";
+import React, { useContext } from 'react'
+import ThemedButton from './ThemedButton'
+import DarkModeToggle from './DarkModeToggle'
+import defaultUser from '../data'
+//import UserProvider for user context
+import { UserContext } from '../context/user'
+import { ThemeContext } from '../context/theme'
 
-function Header({ theme, setTheme, user, setUser }) {
-  function handleLogin() {
-    if (user) {
-      setUser(null);
-    } else {
-      setUser(defaultUser);
-    }
-  }
+function Header() {
+	const { user, setUser } = useContext(UserContext)
+	const { theme, setTheme } = useContext(ThemeContext)
+	function handleLogin() {
+		if (user) {
+			setUser(null)
+		} else {
+			setUser(defaultUser)
+		}
+	}
 
-  return (
-    <header>
-      <h1>React Context</h1>
-      <nav>
-        <ThemedButton onClick={handleLogin} theme={theme}>
-          {user ? "Logout" : "Login"}
-        </ThemedButton>
-        <DarkModeToggle theme={theme} setTheme={setTheme} />
-      </nav>
-    </header>
-  );
+	return (
+		<header>
+			<h1>React Context</h1>
+			<nav>
+				<ThemedButton onClick={handleLogin} theme={theme}>
+					{user ? 'Logout' : 'Login'}
+				</ThemedButton>
+				<DarkModeToggle theme={theme} setTheme={setTheme} />
+			</nav>
+		</header>
+	)
 }
 
-export default Header;
+export default Header
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,19 @@
-import React from "react";
-import Interests from "./Interests";
+import React, { useContext } from 'react'
+import Interests from './Interests'
+// import the UserContext we created
+import { UserContext } from '../context/user'
+import { ThemeContext } from '../context/theme'
 
-function Profile({ user, theme }) {
-  if (!user) return <h2>Please Login To View Profile</h2>;
-  return (
-    <div>
-      <h2>{user.name}'s Profile</h2>
-      <Interests interests={user.interests} theme={theme} />
-    </div>
-  );
+function Profile() {
+	// call useContext with our UserContext
+	const { user } = useContext(UserContext)
+	const { theme } = useContext(ThemeContext)
+	if (!user) return <h2>Please Login To View Profile</h2>
+	return (
+		<div>
+			<h2>{user.name}'s Profile</h2>
+			<Interests interests={user.interests} theme={theme} />
+		</div>
+	)
 }
-
-export default Profile;
+export default Profile
